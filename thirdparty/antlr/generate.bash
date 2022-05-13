24a30c7d2ceb435833a2b4aafd92371853023150
daterletskiy <daterletskiy@gmail.com>
2022-05-13 16:09:13 +0300

    thirdparty update

---
+++ thirdparty/antlr/generate.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+ANTLR4_JAR=${SCRIPT_DIR}/"antlr-4.10.1-complete.jar"
+ANTLR4_OPTIONS="-Dlanguage=Python3 -listener -visitor"
+ANTLR4="java -jar ${ANTLR4_JAR} ${ANTLR4_OPTIONS}"
+
+
+
+G4_FILE=${1}
+OUTPUT_DIR=${2}
+
+${ANTLR4} -o ${OUTPUT_DIR} ${G4_FILE}
---
+++ thirdparty/plantuml/generate.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+PLANT_UML_JAR=${SCRIPT_DIR}/"plantuml-1.2021.14.jar"
+PLANT_UML_OPTIONS="-tsvg -progress -V -debugsvek"
+PLANT_UML="java -jar ${PLANT_UML_JAR} ${PLANT_UML_OPTIONS}"
+
+
+
+SOURCE_DIR=${1}
+OUTPUT_DIR=${2}
+
+# rm ${OUTPUT_DIR}/*.svg
+${PLANT_UML} -o ${OUTPUT_DIR} ${SOURCE_DIR}/*
