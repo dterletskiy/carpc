941565b14dc38a459be18def0132c70727ddff67
Dmytro Terletskyi <dmytro_terletskyi@epam.com>
2023-10-12 12:24:49 +0300

    version 3.13.10

--- runtime/api/carpc/helpers/functions/print.hpp
+++ runtime/api/carpc/helpers/functions/print.hpp
@@ -51,7 +51,6 @@ namespace carpc {
     * Print byte buffer
     * 
     ***************************************************************************************************/
-   // void print( const void* p_buffer, const std::size_t size, const bool is_new_line = false );
    void print_hex( const void* p_buffer, const std::size_t size, const bool is_new_line = false );
    void print_bin( const void* p_buffer, const std::size_t size, const bool is_new_line = false );
 
--- runtime/imp/carpc/helpers/functions/print.cpp
+++ runtime/imp/carpc/helpers/functions/print.cpp
@@ -5,14 +5,6 @@
 
 namespace carpc {
 
-   // void print( const void* const p_buffer, const std::size_t size, const bool is_new_line )
-   // {
-   //    for( std::size_t i = 0; i < size; ++i )
-   //       printf( "%#x ", static_cast< const std::uint8_t* const >( p_buffer )[i] );
-
-   //    if( is_new_line ) printf( "\n" );
-   // }
-
    void print_hex( const void* const p_buffer, const std::size_t size, const bool is_new_line )
    {
       for( std::size_t i = 0; i < size; ++i )
@@ -23,10 +15,18 @@ namespace carpc {
 
    void print_bin( const void* const p_buffer, const std::size_t size, const bool is_new_line )
    {
-      for( std::size_t i = 0; i < size; ++i )
+      static const char* const nibble_representation[16] = {
+         [ 0] = "0000", [ 1] = "0001", [ 2] = "0010", [ 3] = "0011",
+         [ 4] = "0100", [ 5] = "0101", [ 6] = "0110", [ 7] = "0111",
+         [ 8] = "1000", [ 9] = "1001", [10] = "1010", [11] = "1011",
+         [12] = "1100", [13] = "1101", [14] = "1110", [15] = "1111",
+      };
+
+      const std::uint8_t* p_byte = static_cast< const std::uint8_t* >( p_buffer );
+      for( std::size_t i = 0; i < size; ++i, ++p_byte )
       {
-         std::bitset< 8 > x( static_cast< const std::uint8_t* const >( p_buffer )[i] );
-         std::cout << x << " ";
+         std::uint8_t byte = *p_byte;
+         printf( "%s%s", nibble_representation[ byte >> 4 ], nibble_representation[ byte & 0x0F ] );
       }
 
       if( is_new_line ) printf( "\n" );
